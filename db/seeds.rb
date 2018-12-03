b3d798ec833a31c4d97a5ccd925ed1c697df9357
Alex Bennett <benedictus.ab@gmail.com>
2018-12-03 16:41:13 -0700

    added quotes to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,86 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Quote.delete_all
+
+Quote.create! (
+  [
+    {
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde"
+    },
+    {
+      text: "Two things are infinite: the universe and human stupidity; "
+
+            "and I'm not sure about the universe.",
+      author: "Albert Einstein"
+    },
+    {
+      text:  "Be the change that you wish to see in the world",
+      author: "Mahatma Gandhi"
+    },
+    {
+      text: "If you tell the truth, you don't have to remember anything.",
+      author: "Mark Twain"
+    },
+    {
+      text: "Life is 10% what happens to you and 90% how you react to it.",
+      author: "Charles R. Swindoll"
+    },
+    {
+      text: "Don't ever, for any reason, do anything for anyone, for any reason, ever, no matter what. No matter where. Or who, or who you are with, or where you are going or... or where you've been... ever. For any reason, whatsoever.",
+      author: "Michael Scott(Steve Carrell)"
+    },
+    {
+      text: "Life has no limitations, except the ones you make.",
+      author: "Les Brown"
+    },
+    {
+      text: "It is not in the stars to hold our destiny but in ourselves.",
+      author: "William Shakespeare"
+    },
+    {
+      text: "There is no great genuis without a mixture of madness.",
+      author: "Aristotle"
+    },
+    {
+      text: "The first to apologize is the bravest. The first to forgive is the strongest. The first to forget is the happiest.",
+      author: "Unknown"
+    },
+    {
+      text: "Who looks outside, dreams; who looks inside, awakes.",
+      author: "Carl Jung"
+    },
+    {
+      text: "Difficult roads often lead to beautiful destinations.",
+      author: "Unknown"
+    },
+    {
+      text: "Although the world is full of suffering, it is also full of the overcoming of it.",
+      author: "Helen Keller"
+    },
+    {
+      text: "Not all who wander are lost.",
+      author: "J.R.R. Tolkien"
+    },
+    {
+      text: "Faith. You don't need to see the whole staircase, just take the first step.",
+      author: "Martin Luther King Jr."
+    },
+    {
+      text: "Life isn't about finding yourself. Life is about creating yourself.",
+      author: "George Bernard Shaw"
+    },
+    {
+      text: "Your mind is a powerful thing. When you fill it with positive thoughts, your life will start to change.",
+      author: "Unknown"
+    },
+    {
+      text: "For I reckon that the sufferings of this present time are not worthy to be compared with the glory which shall be revealed in us."
+      author: "Romans 8:18"
+    },
+    {
+      text: "The two most important days in your life are the day you are born and the day you find out why.",
+      author: "Mark Twain"
+    }
+  ]
+)
+
+puts "Quotes Seeded!"
